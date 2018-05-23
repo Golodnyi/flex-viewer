a9b73e30e406f37dd6069cb59f61bb4447556aab
Golodnyi <ochen@golodnyi.ru>
2018-05-23 15:11:40 +0700

    read binary data to vector

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 extern crate scan_dir;
 use scan_dir::ScanDir;
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() {
   let path = std::env::args()
@@ -9,11 +11,21 @@ fn main() {
 
   println!("path: {}", path);
 
+  let mut data: Vec<u8> = vec![];
   ScanDir::files()
     .read(path, |files| {
       for (entry, name) in files {
-        println!("File {:?} has full path {:?}", name, entry.path());
+        println!("read: {}", name);
+        data = read_binary_file(entry.path());
       }
     })
-    .unwrap()
+    .unwrap();
+
+}
+
+fn read_binary_file(file: std::path::PathBuf) -> Vec<u8> {
+  let mut data: Vec<u8> = vec![];
+  let mut file = File::open(file).expect("open file error");
+  file.read_to_end(&mut data).expect("read error");
+  data
 }
