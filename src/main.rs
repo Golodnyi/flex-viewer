8b2b8e370526ab3d7986c6630e7cc3e2c6d642c4
Golodnyi <ochen@golodnyi.ru>
2018-05-23 15:22:48 +0700

    print bitfield

--- src/main.rs
+++ src/main.rs
@@ -16,16 +16,25 @@ fn main() {
     .read(path, |files| {
       for (entry, name) in files {
         println!("read: {}", name);
-        data = read_binary_file(entry.path());
+        for byte in read_binary_file(entry.path()) {
+          data.push(byte);
+        }
+        print_binary_data(&data);
       }
     })
     .unwrap();
-
 }
 
 fn read_binary_file(file: std::path::PathBuf) -> Vec<u8> {
-  let mut data: Vec<u8> = vec![];
+  let mut data = vec![];
   let mut file = File::open(file).expect("open file error");
   file.read_to_end(&mut data).expect("read error");
   data
+}
+
+fn print_binary_data(data: &Vec<u8>) {
+  print!("Bitfield:");
+  for bit in data[..85].iter() {
+    print!("{:?} ", bit);
+  }
 }
