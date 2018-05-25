e6d6bcc4ec6cbaa649a9bd7d0fa78cdc190d9ecc
Golodnyi <ochen@golodnyi.ru>
2018-05-25 16:41:42 +0700

    Set default logs path, if not set.

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,18 @@ extern crate serde_derive;
 mod reader;
 mod flex;
 use std::path::PathBuf;
-
+use std::env::current_exe;
 fn main() {
     let mut path = PathBuf::new();
-    path.push(std::env::args().skip(1).next()
-        .expect("usage: flex-viewer PATH")
-    );
+    match std::env::args().skip(1).next() {
+        Some(p) => {
+            path.push(p);
+        }
+        None => {
+            let current_exe = current_exe().unwrap();
+            path.push(current_exe.join("logs"));
+        }
+    }
 
     println!("path: {:?}", path);
     let files = reader::read_dir(path).expect("Cannot read dir");
