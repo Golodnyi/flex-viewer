772d7b868407f8ebad7e51280bced066c748df3c
Golodnyi <ochen@golodnyi.ru>
2018-06-04 16:36:23 +0700

    Progressbar

--- src/bin/flex-viewer.rs
+++ src/bin/flex-viewer.rs
@@ -23,40 +23,50 @@ fn main() {
     }
 
     match fs::remove_file("report.html") {
-        Err(e) => {
-            println!("Error: {}", e);
-        },
+        Err(_e) => {
+        }
         _ => {}
     }
 
     let files = reader::read_dir(path).expect("Cannot read dir");
 
-    for file in files.iter() {
+    println!("0%");
+
+    for (i, file) in files.iter().enumerate() {
         let data = match reader::read_binary_file(file) {
             Ok(data) => data,
-            Err(e) => {
-                println!("Error: {}", e);
+            Err(_e) => {
                 continue;
             }
         };
         match flex::parse(data) {
-            Err(e) => {
-                println!("Error: {}", e);
+            Err(_e) => {
                 continue;
-            },
+            }
             Ok(flex) => {
                 let mut flex: Vec<Flex> = Vec::from(flex);
                 match html::append(&mut flex) {
-                    Err(e) => {
-                        println!("Error: {}", e);
+                    Err(_e) => {
                         continue;
-                    },
+                    }
                     Ok(table) => {
                         tables.push(table);
                     }
                 }
             }
         };
+        let progress: usize = ((i as f32 + 1.0) / files.len() as f32 * 100.0) as usize;
+        println!("{}[2J", 27 as char);
+        let mut symbols: String = "".to_string();
+
+        for p in 0..100 {
+            if p <= progress {
+                symbols.push_str("+");
+            } else {
+                symbols.push_str(" ");
+            }
+        }
+        println!("{}% [{}]", progress, symbols);
     }
 
     html::generate(&tables).expect("Error: generate report");
